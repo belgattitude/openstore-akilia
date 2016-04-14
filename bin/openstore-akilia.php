4656bf8deaae9e70d80727b6fb820b523b46c26d
Sébastien Vanvelthem <belgattitude@gmail.com>
2016-04-14 14:18:01 +0200

    Updated

--- bin/openstore-akilia.php
+++ bin/openstore-akilia.php
@@ -55,35 +55,12 @@ $cli = new Symfony\Component\Console\Application('openstore-schema-core console'
 $cli->setCatchExceptions(true);
 // commands
 $cli->addCommands(array(
-    // DBAL Commands
-    // 
-    //new Doctrine\DBAL\Tools\Console\Command\RunSqlCommand(),
-    //new Doctrine\DBAL\Tools\Console\Command\ImportCommand(),
-
-    // ORM Commands
-    new Doctrine\ORM\Tools\Console\Command\ClearCache\MetadataCommand(),
-    new Doctrine\ORM\Tools\Console\Command\ClearCache\ResultCommand(),
-    new Doctrine\ORM\Tools\Console\Command\ClearCache\QueryCommand(),
-    new Doctrine\ORM\Tools\Console\Command\SchemaTool\CreateCommand(),
-    new Doctrine\ORM\Tools\Console\Command\SchemaTool\UpdateCommand(),
-    new Doctrine\ORM\Tools\Console\Command\SchemaTool\DropCommand(),
-    //new Doctrine\ORM\Tools\Console\Command\EnsureProductionSettingsCommand(),
-    //new Doctrine\ORM\Tools\Console\Command\ConvertDoctrine1SchemaCommand(),
-    new Doctrine\ORM\Tools\Console\Command\GenerateRepositoriesCommand(),
-    new Doctrine\ORM\Tools\Console\Command\GenerateEntitiesCommand(),
-    new Doctrine\ORM\Tools\Console\Command\GenerateProxiesCommand(),
-    //new Doctrine\ORM\Tools\Console\Command\ConvertMappingCommand(),
-    new Doctrine\ORM\Tools\Console\Command\RunDqlCommand(),
-    new Doctrine\ORM\Tools\Console\Command\ValidateSchemaCommand(),
-    new Doctrine\ORM\Tools\Console\Command\InfoCommand(),
     
     new OpenstoreSchema\Core\Tools\Console\Command\Schema\CreateCommand(),
     new OpenstoreSchema\Core\Tools\Console\Command\Schema\RecreateExtraCommand(),
     new OpenstoreSchema\Core\Tools\Console\Command\Schema\UpdateCommand(),
     new OpenstoreSchema\Core\Tools\Console\Command\Schema\DropCommand()
 ));
-
-
 
 // helpers
 $helpers = array(
